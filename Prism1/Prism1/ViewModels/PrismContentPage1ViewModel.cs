f0f4aaef1f80dd3fdf1311867434d0bfd75be8b6
Jonathan Allen <jallen@snow.edu>
2019-04-12 11:24:29 -0600

    navigation with parameters

--- Prism1/Prism1/ViewModels/PrismContentPage1ViewModel.cs
+++ Prism1/Prism1/ViewModels/PrismContentPage1ViewModel.cs
@@ -1,16 +1,31 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Prism1.ViewModels
 {
-    public class PrismContentPage1ViewModel : BindableBase
+    public class PrismContentPage1ViewModel : ViewModelBase
     {
-        public PrismContentPage1ViewModel()
+        public PrismContentPage1ViewModel(INavigationService navigationService)
+            : base(navigationService)
         {
 
         }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            EntryValue = (string)parameters["entry"];
+        }
+
+        private string entryValue;
+        public string EntryValue
+        {
+            get => entryValue;
+            set { SetProperty(ref entryValue, value); }
+        }
+
     }
 }
