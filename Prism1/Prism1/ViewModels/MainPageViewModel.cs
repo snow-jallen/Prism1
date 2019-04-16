86ca538b80a4b3d89ab30d52e0f0066f631e07be
Jonathan Allen <jallen@snow.edu>
2019-04-16 14:50:25 -0600

    Demonstrate messagebox calls

--- Prism1/Prism1/ViewModels/MainPageViewModel.cs
+++ Prism1/Prism1/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using Prism1.Views;
 using System;
 using System.Collections.Generic;
@@ -15,21 +16,42 @@ namespace Prism1.ViewModels
         private ICommand goToContentPage;
         private ICommand goToTabbedPage;
 
-        public MainPageViewModel(INavigationService navigationService)
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
             : base(navigationService)
         {
             Title = "Main Page";
+            this.dialogService = dialogService ?? throw new ArgumentNullException(nameof(dialogService));
         }
 
-        public ICommand GoToTabbedPage => goToTabbedPage ?? (goToTabbedPage = new DelegateCommand(() => NavigationService.NavigateAsync(nameof(PersonListView))));
+        public ICommand GoToTabbedPage => goToTabbedPage ?? (goToTabbedPage = new DelegateCommand(async () =>
+        {
+            var result = await dialogService.DisplayActionSheetAsync("Action Sheet Title", "Don't do it!", "DESTROY!!!", "Other Option #3", "Other Option #4", "Really crazy long Option #5\nwith Multiple Lines");
+            if (result == "Don't do it!")
+            {
+                await dialogService.DisplayActionSheetAsync("Using custom action buttons",
+                    ActionSheetButton.CreateCancelButton("Run Away!", () => EntryValue = "Run AWAY!!!"),
+                    ActionSheetButton.CreateDestroyButton("Do you feel lucky?", () => EntryValue = "Well do you?"),
+                    ActionSheetButton.CreateButton("3rd Alternative", () => EntryValue = "You don't actually think you have a choice do you?"));
+            }
+
+            await NavigationService.NavigateAsync(nameof(PersonListView));
+        }));
+
         public ICommand GoToContentPage => goToContentPage ?? (goToContentPage = new DelegateCommand(async () =>
         {
+            var result = await dialogService.DisplayAlertAsync("Alert Title", "Alert Message", "Okey Dokey", "Negatory");
+
+            if (!result)
+                return;
+
             var p = new NavigationParameters();
             p.Add("entry", EntryValue);
             await NavigationService.NavigateAsync(nameof(PersonEntryView), p);
         }));
 
         private string entryValue;
+        private readonly IPageDialogService dialogService;
+
         public string EntryValue
         {
             get => entryValue;
